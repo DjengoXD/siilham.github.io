3eb1ad60bfcaa68df09cdbc6224b118a70bdf6c2
DjengoXD <93562913+DjengoXD@users.noreply.github.com>
2023-11-16 00:55:03 +0700

    Add files via upload

---
+++ 11220133/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Landing Page CV</title>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+
+<!-- Header -->
+<header class="bg-dark text-white text-center py-5">
+    <h1 class="display-4">Nama Anda</h1>
+    <p class="lead">Deskripsi singkat tentang diri Anda</p>
+</header>
+
+<!-- Container untuk layout -->
+<div class="container mt-4">
+    <div class="row">
+
+        <!-- Menu di sebelah kiri -->
+        <div class="col-md-3">
+            <div class="list-group">
+                <a href="#brand" class="list-group-item list-group-item-action">Branda</a>
+                <a href="#cv" class="list-group-item list-group-item-action">CV</a>
+                <a href="#gallery" class="list-group-item list-group-item-action">Galeri</a>
+            </div>
+        </div>
+
+        <!-- Konten di tengah -->
+        <div class="col-md-9">
+            <!-- Isi web Anda -->
+            <div class="jumbotron">
+                <h2>Selamat datang di CV Saya</h2>
+                <p>Deskripsi lebih lanjut tentang diri Anda dan pengalaman Anda.</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Footer -->
+<footer class="bg-dark text-white text-center py-3">
+    <p>Hak Cipta © 2023 Nama Anda</p>
+</footer>
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.2/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+</body>
+</html>
